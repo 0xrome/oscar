fa6a851f8ca2c6f3617c4cfd1f7de787ee1792b2
0xrome <joshromanrogers@gmail.com>
2023-10-29 15:29:38 +0000

    Firebase config and show users on Users page

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,3 +17,33 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import functions = require("firebase-functions");
+import express = require("express");
+import bodyParser = require("body-parser");
+import admin = require("firebase-admin");
+
+admin.initializeApp();
+const app = express();
+
+// Use body-parser middleware to parse the incoming JSON
+app.use(bodyParser.json());
+
+app.post("/typeform-webhook", async (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  req: { body: any; }, res: { sendStatus: (arg0: number) => void; }) => {
+  const formResponse = req.body;
+
+  // Ensure it's a valid Typeform payload
+  if (formResponse && formResponse.form_response) {
+    // Store the data in Firebase
+    const db = admin.firestore();
+    const docRef = db.collection("typeformResponses").doc();
+    await docRef.set(formResponse.form_response);
+  }
+
+  // Send a response to acknowledge receipt of the data
+  res.sendStatus(200);
+});
+
+exports.typeformWebhook = functions.https.onRequest(app);
--- oscar-fe/src/config/firebaseConfig.ts
+++ oscar-fe/src/config/firebaseConfig.ts
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyAIZaBLrWGI_8rUE0BQVysiyui1EU6j8-c",
   authDomain: "oscar-da63e.firebaseapp.com",
@@ -22,5 +18,6 @@ export const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const db = getFirestore(app); // Initializing Firestore here
 
-export { auth };
+export { auth, db }; // Exporting Firestore instance as 'db'
