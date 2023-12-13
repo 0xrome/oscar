9c190e045c042b249add9bd61ec2a23918c4409c
0xrome <joshromanrogers@gmail.com>
2023-12-13 11:49:18 +0000

    Fix createWeeklySurvey

--- functions/src/createWeeklySurvey.ts
+++ functions/src/createWeeklySurvey.ts
@@ -3,30 +3,84 @@ import * as admin from 'firebase-admin';
 import axios from 'axios';
 
 export const createWeeklySurvey = functions.https.onRequest(async (req, res) => {
-    // Define the structure of the new survey
-    const survey = {
-        title: 'Availability Survey',
-        hidden: ['email'], // This will add a hidden email field
-        fields: [
-            {
-                title: 'Which days are you available next week?',
-                type: 'multiple_choice',
-                ref: 'availability_next_week',
-                choices: getChoicesForNextWeek().concat({ label: 'None of these dates' })
+    try {
+        const typeformApiKey = functions.config().typeform.key;
+        var surveyRef: any;
+        var formId: any;
+
+        // Retrieve the theme ID
+        const themeResponse = await axios.get('https://api.typeform.com/themes', {
+            headers: { Authorization: `Bearer ${typeformApiKey}` },
+            params: { page_size: 200 }
+        });
+
+        const themes = themeResponse.data.items;
+        const theme = themes.find((theme: { name: string; }) => theme.name === 'Availability Survey');
+        console.log('Theme:', theme);
+        if (!theme) {
+            throw new Error('Theme not found');
+        }
+
+
+        // Define the structure of the new survey
+        const survey = {
+            title: `Availability Survey (${getDate()})`,
+            hidden: ['email'], // This will add a hidden email field
+            theme: {
+                href: `https://api.typeform.com/themes/${theme.id}`
             },
-            {
-                title: 'Which days are you available the week after next?',
-                type: 'multiple_choice',
-                ref: 'availability_week_after_next',
-                choices: getChoicesForWeekAfterNext()
-            },
-            {
-                title: 'Thank you for your response!',
-                type: 'statement',
-                ref: 'thank_you'
-            }
-        ],
-        logic: [
+            fields: [
+                {
+                    title: 'Which days are you available next week?',
+                    type: 'multiple_choice',
+                    ref: 'availability_next_week',
+                    properties: {
+                        description: 'Please select all that apply.',
+                        randomize: false,
+                        allow_multiple_selection: true,
+                        allow_other_choice: false,
+                        vertical_alignment: true,
+            choices: getChoicesForNextWeek().concat([{ label: 'None of these dates' }])
+                    }
+                },
+                {
+                    title: 'Which days are you available the week after next?',
+                    type: 'multiple_choice',
+                    ref: 'availability_week_after_next',
+                    properties: {
+                        description: 'Please select all that apply.',
+                        randomize: false,
+                        allow_multiple_selection: true,
+                        allow_other_choice: false,
+                        vertical_alignment: true,
+                        choices: getChoicesForWeekAfterNext().concat([{ label: 'None of these dates' }])
+                    }
+                },
+                {
+                    title: 'Thank you for your response!',
+                    type: 'statement',
+                    ref: 'thank_you'
+                }
+            ],
+        };
+
+        // Use the Typeform Create API to create the new survey
+        const createResponse = await axios.post('https://api.typeform.com/forms', survey, {
+            headers: { Authorization: `Bearer ${typeformApiKey}` }
+        });
+        formId = createResponse.data.id;
+        surveyRef = createResponse;
+
+        // Retrieve the form definition
+        const formResponse = await axios.get(`https://api.typeform.com/forms/${formId}`, {
+            headers: { 'Authorization': `Bearer ${typeformApiKey}` }
+        });
+        const form = formResponse.data;
+        const field = form.fields.find((field: { ref: string; }) => field.ref === 'availability_next_week');
+        const refId = field.properties.choices[field.properties.choices.length - 1].ref;
+
+        // Add the logic condition
+        form.logic = [
             {
                 type: 'field',
                 ref: 'availability_next_week',
@@ -34,50 +88,51 @@ export const createWeeklySurvey = functions.https.onRequest(async (req, res) => 
                     {
                         action: 'jump',
                         details: {
-                            to: 'availability_week_after_next'
+                            to: {
+                                type: 'field',
+                                value: 'thank_you'
+                            }
                         },
                         condition: {
-                            op: 'is',
+                            op: 'is_not',
                             vars: [
                                 { type: 'field', value: 'availability_next_week' },
-                                { type: 'choice', value: 'None of these dates' }
+                                { type: 'choice', value: refId }
                             ]
                         }
                     }
                 ]
             },
-            {
-                type: 'field',
-                ref: 'availability_week_after_next',
-                actions: [
-                    {
-                        action: 'jump',
-                        details: {
-                            to: 'thank_you'
-                        }
-                    }
-                ]
-            }
         ]
-    };
 
-    // Use the Typeform Create API to create the new survey
-    const response = await axios.post('https://api.typeform.com/forms', survey, {
-        headers: { Authorization: `Bearer ${process.env.TYPEFORM_API_KEY}` }
-    });
+        // Update the form
+        await axios.put(`https://api.typeform.com/forms/${formId}`, form, {
+            headers: { 'Authorization': `Bearer ${typeformApiKey}` }
+        });
 
-    // Get the URL of the created survey
-    const surveyUrl = response.data._links.display;
+        console.log('Form updated successfully');
+        const surveyUrl = surveyRef.data._links.display;
 
-    // Initialize Firestore
-    const db = admin.firestore();
+        // Initialize Firestore
+        const db = admin.firestore();
 
-    // Save the survey URL in a document in your Firestore database
-    await db.collection('Surveys').doc('current').set({ url: surveyUrl });
+        // Save the survey URL in Firestore
+        await db.collection('Surveys').doc('current').set({ url: surveyUrl });
 
-    // At the end of your function, send a response to indicate that the function has completed
-    res.status(200).send('Survey created successfully');
+        res.status(200).send('Survey created successfully');
+    } catch (error:any) {
+        console.error('Error creating survey:', error.message);
+        if (error.response) {
+            console.error('Response data:', error.response.data);
+        }
+        res.status(500).send('Error creating survey');
+    }
 });
+
+function getDate() {
+    const today = new Date();
+    return today.toLocaleDateString('default', { month: 'numeric', day: 'numeric' });
+}
 
 function getChoicesForNextWeek() {
     const choices = [];
--- utils/mailchimpSetup.ts
+++ utils/mailchimpSetup.ts
@@ -1,5 +1,5 @@
 const mailchimpClient = require("@mailchimp/mailchimp_transactional")(
-    "vxIBxXVU5DyrPKNCdq8c2A"
+    "md-9XQPpNweJEGYDZKvQ6TjEQ"
   );
 
 export default mailchimpClient;
