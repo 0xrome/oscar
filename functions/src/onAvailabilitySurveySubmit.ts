64d949c892e9b5b9526941097d6e30c1b7eef1df
0xrome <joshromanrogers@gmail.com>
2023-12-27 11:53:25 +0000

    availability survey todo update

--- functions/src/onAvailabilitySurveySubmit.ts
+++ functions/src/onAvailabilitySurveySubmit.ts
@@ -14,6 +14,7 @@ const stripe = new Stripe(functions.config().stripe.secret, {
     apiVersion: '2020-08-27',
   });
 
+  // TODO: Build tests
 export const onSurveySubmit = functions.https.onRequest(async (req, res) => {
     // Parse the request body
     const email = req.body.form_response.hidden.email;
@@ -41,7 +42,8 @@ export const onSurveySubmit = functions.https.onRequest(async (req, res) => {
     const otherUserField = isUserA ? 'userBAvailability' : 'userAAvailability';
 
     // Update the status field and availability based on the survey responses
-    const availability = responses[1].text; // Replace with actual logic
+    // TODO: Change availability logic to match the survey response (multiple availabilities possible)
+    const availability = responses[1].text;
 
     if (availability === 'None of these dates') {
         // Cancel the match
@@ -54,6 +56,7 @@ export const onSurveySubmit = functions.https.onRequest(async (req, res) => {
 
         res.status(200).send('Match cancelled.');
     } else {
+        // TODO: Change availability logic to match the survey response (multiple availabilities possible)
         const status = responses[0].text; // Replace with actual logic
         await match.ref.update({ status, [userField]: availability });
 
@@ -68,6 +71,7 @@ export const onSurveySubmit = functions.https.onRequest(async (req, res) => {
 
         if (matchingDates.length > 0) {
             // If there's a match, update the match document with the earliest matching date
+            // TODO: Check to see if sorting logic is correct and if there's a better way to do it
             matchingDates.sort((a: { getTime: () => number; }, b: { getTime: () => number; }) => a.getTime() - b.getTime());
             const matchDate = matchingDates[0];
 
@@ -78,7 +82,9 @@ export const onSurveySubmit = functions.https.onRequest(async (req, res) => {
 
                  // Send payment link email to both users
                 await sendPaymentLinkEmail(matchData.userAEmail, stripePayment);
+                console.log(`Payment link email sent to ${matchData.userAEmail}`);
                 await sendPaymentLinkEmail(matchData.userBEmail, stripePayment);
+                console.log(`Payment link email sent to ${matchData.userBEmail}`);
 
                 // Update the payment status for each user to 'pending'
                 const userPaymentStatusField = isUserA ? 'userAPaymentStatus' : 'userBPaymentStatus';
@@ -94,19 +100,22 @@ export const onSurveySubmit = functions.https.onRequest(async (req, res) => {
               await match.ref.update({ status: 'cancelled' });
               const otherUserEmail = isUserA ? matchData.userBEmail : matchData.userAEmail;
               await sendCancellationEmail(email);
+              console.log(`Cancellation email sent to ${email}`);
               await sendCancellationEmail(otherUserEmail);
+              console.log(`Cancellation email sent to ${otherUserEmail}`);
               res.status(200).send('No match found. Match cancelled.');
             }
     }
 });
 
-// TODO: build out this function
+// TODO: build out getMatchingDates
 function getMatchingDates(userAvailabilityString: string, otherUserAvailabilityString: string) {
     const userAvailability = userAvailabilityString.split(', ').map(date => new Date(date));
     const otherUserAvailability = otherUserAvailabilityString.split(', ').map(date => new Date(date));
     return userAvailability.filter(date => otherUserAvailability.some(otherDate => otherDate.getTime() === date.getTime()));
 }
 
+// TODO: Create email utility function
 async function sendCancellationEmail(email: string) {
     const message = {
         from_email: 'your-email@example.com',
@@ -128,6 +137,7 @@ async function sendCancellationEmail(email: string) {
     }
 }
 
+// TODO: Build out createStripePaymentLink
 async function createStripePaymentLink(): Promise<string> {
     try {
       const session = await stripe.checkout.sessions.create({
@@ -148,6 +158,7 @@ async function createStripePaymentLink(): Promise<string> {
     }
   }
 
+  // TODO: Build out sendPaymentLinkEmail
   async function sendPaymentLinkEmail(email: string, paymentLink: string) {
     const message = {
       from_email: 'your-email@example.com',
