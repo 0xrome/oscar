2db60a39e627331cff737f5c0cf6ad33a26034ae
0xrome <joshromanrogers@gmail.com>
2023-12-27 12:48:21 +0000

    update paymentReminder logic and todos

--- functions/src/reminders/sendPaymentReminderOrCancel.ts
+++ functions/src/reminders/sendPaymentReminderOrCancel.ts
@@ -4,32 +4,52 @@ import axios from 'axios';
 
 import db from '../utils/db';
 
-export const sendPaymentReminderOrCancel = async () => {
-    const now = admin.firestore.Timestamp.now();
-    const twentyFourHoursAgo = moment(now.toDate()).subtract(24, 'hours').toDate();
-    const fortyEightHoursAgo = moment(now.toDate()).subtract(48, 'hours').toDate();
+export const sendPaymentReminder = async () => {
+  const now = admin.firestore.Timestamp.now();
+  const twentyFourHoursAgo = moment(now.toDate()).subtract(24, 'hours').toDate();
+
+    // TODO: Make sure that paymentLinkSentAt or similar + status is indexed in the relevant Matches document when payment link is sent
+    // TODO: Add reminderSent field to Matches document
+  const matchQuery = await db.collection('Matches')
+    .where('paymentLinkSentAt', '<=', twentyFourHoursAgo)
+    .where('status', '==', 'pending')
+    .where('reminderSent', '==', false) // Only select matches where a reminder hasn't been sent
+    .get();
+
+  matchQuery.docs.forEach(async (doc) => {
+    const match = doc.data();
+    const { phoneNumber, paymentLink } = match;
+
+    // Send a reminder if the payment is still pending after 24 hours
+    await sendWhatsappMessage(phoneNumber, `Please complete your payment at ${paymentLink}`);
+
+
+    // Update the document to indicate that a reminder has been sent
+    await db.collection('Matches').doc(doc.id).update({ reminderSent: true });
+  });
+}
+
+export const cancelUnpaidDates = async () => {
+  const now = admin.firestore.Timestamp.now();
+  const fortyEightHoursAgo = moment(now.toDate()).subtract(48, 'hours').toDate();
+
+  const matchQuery = await db.collection('Matches')
+    .where('paymentLinkSentAt', '<=', fortyEightHoursAgo)
+    .where('status', '==', 'pending')
+    .get();
+
+  matchQuery.docs.forEach(async (doc) => {
+    const match = doc.data();
+    const { userAEmail, userBEmail } = match;
+
+    // Cancel the date if the payment is still pending after 48 hours
+    await db.collection('Matches').doc(doc.id).update({ status: 'cancelled' });
+    await sendCancellationEmail(userAEmail, userBEmail); // Send cancellation emails to both users
+  });
+}
+
   
-    const paymentQuery = await db.collection('Payments')
-      .where('paymentLinkSentAt', '>', fortyEightHoursAgo)
-      .where('paymentLinkSentAt', '<=', twentyFourHoursAgo)
-      .where('status', '==', 'pending')
-      .get();
-  
-    paymentQuery.docs.forEach(async (doc) => {
-      const payment = doc.data();
-      const { phoneNumber, paymentLink } = payment;
-  
-      if (moment(payment.paymentLinkSentAt.toDate()).isBefore(fortyEightHoursAgo)) {
-        // Cancel the date if the payment is still pending after 48 hours
-        await db.collection('Dates').doc(payment.dateId).update({ status: 'cancelled' });
-        await sendWhatsappMessage(phoneNumber, 'Your date has been cancelled due to non-payment.');
-      } else {
-        // Send a reminder if the payment is still pending after 24 hours
-        await sendWhatsappMessage(phoneNumber, `Please complete your payment at ${paymentLink}`);
-      }
-    });
-  }
-  
+// TODO: Build whatsapp message, try and use utility function
   const sendWhatsappMessage = async (phoneNumber: string, message: string) => {
     const requestBody = {
       phone: phoneNumber,
@@ -37,3 +57,8 @@ export const sendPaymentReminderOrCancel = async () => {
     };
     await axios.post('https://api.whatsapp.com/send', requestBody);
   };
+
+  // TODO: Build email, try and use utility function
+  const sendCancellationEmail = async (userAEmail: string, userBEmail: string) => {
+    // Send cancellation email to both users
+  }
