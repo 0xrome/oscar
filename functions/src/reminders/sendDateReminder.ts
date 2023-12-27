83a2d61ba6c3ec2271fc047c578b7bc8df68208a
0xrome <joshromanrogers@gmail.com>
2023-12-27 21:17:12 +0000

    update sendDateReminder TODOs

--- functions/src/reminders/sendDateReminder.ts
+++ functions/src/reminders/sendDateReminder.ts
@@ -1,16 +1,25 @@
+import db from "../utils/db";
+
 export const sendDateReminder = async () => {
     const now = admin.firestore.Timestamp.now();
     const twentyFourHoursFromNow = moment(now.toDate()).add(24, 'hours').toDate();
     const twentyThreeHoursFromNow = moment(now.toDate()).add(23, 'hours').toDate();
   
+    // TODO: Update how many hours until date
+    // TODO: Make sure reminderSent, dateStatus, userAPhone and userBPhone are indexed in the Matches collection
     const matchesQuery = await db.collection('Matches')
       .where('date', '>', twentyThreeHoursFromNow)
       .where('date', '<=', twentyFourHoursFromNow)
       .where('reminderSent', '==', false)
       .where('dateStatus', '==', 'confirmed')
       .get();
+
+      if (matchesQuery.empty) {
+        console.log('No matches found that need reminders sent.');
+        return;
+      }
   
-    matchesQuery.docs.forEach(async (doc) => {
+    matchesQuery.docs.forEach(async (doc: { data: () => any; ref: { update: (arg0: { reminderSent: boolean; }) => any; }; }) => {
       const match = doc.data();
       await sendWhatsappMessage(match.userAPhone, `Reminder: You have a date scheduled at ${match.location} on ${match.date.toDate()}.`);
       await sendWhatsappMessage(match.userBPhone, `Reminder: You have a date scheduled at ${match.location} on ${match.date.toDate()}.`);
@@ -18,8 +27,7 @@ export const sendDateReminder = async () => {
     });
   }
   
-  // Define other reminder functions here
-  
+  // TODO: Update to use WA utiltiy function if possible
   const sendWhatsappMessage = async (phoneNumber: string, message: string) => {
     const requestBody = {
       phone: phoneNumber,
